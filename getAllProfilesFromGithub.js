c2b4c77901adc3b3028fd761801682fb8c95290c
Michelle <34873832+kiecoo@users.noreply.github.com>
2018-01-24 22:25:07 +0800

    2-CATCH the error and make a temporary fix

--- getAllProfilesFromGithub.js
+++ getAllProfilesFromGithub.js
@@ -1,10 +1,49 @@
+// getAllProfilesFromGithub.js
+module.exports = getAllProfilesFromGithub
 
+function getAllProfilesFromGithub (callback) {
 
-var dc = require('dom-console', '5.1.0')()
+  var dc = require('dom-console', '5.1.0')()
 dc.api.toggle()
 var minixhr = require('minixhr')
 var bel = require('bel')
 var csjs = require('csjs-inject')
+
+var css = csjs`
+  body{
+    background-color : pink;
+    color:black;
+}
+.card{
+   width : 300px;
+   height : 300px;
+   border : 5px solid white;
+}
+.front {
+    background-color:    lightblue;
+    height: 300px;
+    animation-name: flip2;
+    animation-duration: 5s;
+  }
+  .back {
+      background-color: pink;
+      height: 200px;
+    animation-name: flip1;
+    animation-duration: 1s;
+  }
+@keyframes flip1 {
+    from {height: 300px;}
+    to {height: 600px;}
+  }
+  @keyframes flip2 {
+    from {height: 600px;}
+    to {height: 300px;}
+  }
+`
+
+
+
+
 
 var url = 'https://api.github.com/repos/wizardamigos/profiles/forks'  
 
@@ -12,8 +51,8 @@ minixhr(url, response)
 
 function response (json) {
   var allusers = JSON.parse(json)
-  console.info(allusers) 
-  
+  // console.info(allusers) 
+
   allusers.forEach(function (nameEach) {
     var reponame = nameEach.full_name
     var repoURL = 'https://api.github.com/repos/' + reponame
@@ -39,10 +78,16 @@ function response2 (data) {
           minixhr(configJsonURL, function (data) {
             var obj = JSON.parse(data)
             var json = atob(obj.content)
-            var profile = JSON.parse(json)
-            
+            try {
+              var profile = JSON.parse(json)            
+            } catch (error) {
+              profile = {
+                name: `ERROR: ${configJsonURL}`
+              }
+            }
+
             console.log(profile)
-            
+
             document.body.appendChild(usercard(profile))
             // document.body.appendChild(usercard(profile))
             // document.body.appendChild(usercard(profile))
@@ -52,25 +97,33 @@ function response2 (data) {
     })
   })  
 }
-var css = csjs`
-  .main { color: red; }
-  .json {
-    background-color: grey;
-    color: white;
-  }
-`
+
 function usercard (profile) {
-  return bel`
-    <div class=${css.main}>
-      <h1> ${profile.username} </h1>
-      <pre class=${css.json}> ${ JSON.stringify(profile, null, 4) } </pre>
-    </div>
-  `
+
+  function usecardA(data){
+  var el = bel`
+  <div onclick="${flip}" class="${css.card} ${css.front}"> 
+      <h1>${data.name}</h1>
+      <img scr="smiley.gif" alt="photo" height="200" width="200">
+  </div>
+`
+  function flip (){
+    el.classList.toggle(css.front)
+    el.classList.toggle(css.back)
+  }
+  return el
 }
 
-// // allusers.forEach(function(data)){
-//    var name = data.owner.login
-//    console.info(name) 
-// }
+  return bel`
+  <div > 
+      <h1>${usecardA(profile) }</h1>
+  </div>
 
-// owner.login
+`
+}
+  
+  
+  var error = null // or `new Error('something went wrong')
+  var allUserProfiles = profile // write code to add all the data
+  callback(error, allUserProfiles)
+}
